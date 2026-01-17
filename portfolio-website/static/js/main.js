99c41d1dc3fd04557b341fb755c52335a7d0ca1a
Ada <adaboddicker@proton.me>
2026-01-16 20:15:30 -0600

    Created Basic Theme Switcher

--- portfolio-website/static/js/main.js
+++ portfolio-website/static/js/main.js
@@ -1,3 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('JavaScript Loaded');
-})
+
+    // theme toggle
+    const themeToggle = document.getElementById('theme-toggle');
+    
+    themeToggle.addEventListener('click', () => {
+        document.documentElement.classList.toggle('proton');;
+    });
+});
