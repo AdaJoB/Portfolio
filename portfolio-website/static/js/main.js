5497d2e017248eeafb3be95bf069de532c4df3ed
Ada <adaboddicker@proton.me>
2026-02-11 19:12:13 -0600

    Remove theme menu after mouseleave

--- portfolio-website/static/js/main.js
+++ portfolio-website/static/js/main.js
@@ -7,11 +7,31 @@ document.addEventListener('DOMContentLoaded', () => {
     const themeButtons = document.querySelectorAll('#theme-selector button');
     const themes = ['light', 'proton', 'forest', 'portal', 'peaks'];
     
+    let isHoveringTheme = false;
+
+    // opens the dropdown menu
     themeToggle.addEventListener('click', (e) => {
         e.stopPropagation();
-        themeDropdown.classList.toggle('open');
+        themeDropdown.classList.add('open');
+        
     });
 
+    // removes dropdown menu after mouse is off for 300ms
+    themeDropdown.addEventListener('mouseenter', () => {
+        isHoveringTheme = true;
+    });
+
+    themeDropdown.addEventListener('mouseleave', () => {
+        isHoveringTheme = false;
+        timeout = setTimeout(() => {
+            if (!isHoveringTheme) {
+                themeDropdown.classList.remove('open');
+            }
+        }, 300);
+    });
+
+
+    // theme button functionality in menu
     themeButtons.forEach(button => {
         button.addEventListener('click', () => {
             document.documentElement.classList.remove(...themes);
