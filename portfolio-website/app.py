c3693c774e8394ce03ef8409f24ffda556a09070
AdaJoB <ada.b.dev@pm.me>
2026-07-07 23:58:00 -0500

    Created blog page

--- portfolio-website/app.py
+++ portfolio-website/app.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///portfolio.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class PostTitles(db.Model):
+    __tablename__ = 'title'
+    title_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('landing/index.jinja')
+
+@app.route('/blog')
+def blog():
+    blog_posts = PostTitles.query.all()
+    return render_template('blog/index.jinja', title=blog_posts)
 
 if __name__ == '__main__':
     app.run(debug=True)
