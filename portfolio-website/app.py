e546a3af813bbc4320f87df90b391adab1b1eace
AdaJoB <ada.b.dev@pm.me>
2026-07-08 19:14:47 -0500

    Created make post page

--- portfolio-website/app.py
+++ portfolio-website/app.py
@@ -6,10 +6,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///portfolio.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class PostTitles(db.Model):
-    __tablename__ = 'title'
-    title_id = db.Column(db.Integer, primary_key=True)
+class Blog_Posts(db.Model):
+    __tablename__ = 'post'
+    post_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255))
+    body = db.Column(db.String(255))
+    date = db.Column(db.String(255))
 
 @app.route('/')
 def home():
@@ -17,8 +19,27 @@ def home():
 
 @app.route('/blog')
 def blog():
-    blog_posts = PostTitles.query.all()
-    return render_template('blog/index.jinja', title=blog_posts)
+    posts = Blog_Posts.query.all()
+    return render_template('blog/index.jinja', post=posts)
+
+@app.route('/make-post', methods=['GET', 'POST'])
+def make_post():
+    if request.method == 'POST':
+        post_title = request.form.get('post-title')
+        post_body = request.form.get('post-body')
+        post_date = request.form.get('post-date')
+        print(post_title)
+        print(post_body)
+        print(post_date)
+        
+
+        new_entry = Blog_Posts(title=post_title, body=post_body, date=post_date)
+        db.session.add(new_entry)
+        db.session.commit()
+
+
+    return render_template('make-post/index.jinja')
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
